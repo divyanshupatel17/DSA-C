59878527460c0d52735db9cc89bf2df1840f21c3
Divyanshu Patel <147162942+divyanshupatel17@users.noreply.github.com>
2024-09-30 07:39:16 +0530

    Create LL_functions.c

---
+++ DSA IMP/LinkedList/LL_functions.c
@@ -0,0 +1,20 @@
+
+// 1.   createNode
+
+// 2.a  insertAtBeginning
+// 2.b  insertAtEnd
+// 2.c  insertAtPosition
+// 2.d  insertAfterNode
+
+// 3.a  deleteAtBeginning
+// 3.b  deleteAtEnd
+// 3.c  deleteAtPosition
+// 3.d  deleteNode
+
+// 4.   searchNode
+// 5.   traversalList
+// 6.   lengthOfList
+// 7.   reverseList
+// 8.   findMiddleNode
+// 9.   detectLoop
+// 10.  removeDuplicates
