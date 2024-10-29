2fa16d9e8f0aedaf18ade997094f2c5c35a5b20c
Divyanshu Patel <147162942+divyanshupatel17@users.noreply.github.com>
2024-10-29 17:46:31 +0000

    k

--- LAB13-BFS-and-DFS/1-BFS-and-DFS-5-que.c
+++ LAB13-BFS-and-DFS/1-BFS-and-DFS-5-que.c
@@ -1,3 +1,13 @@
+Summary of BFS and DFS Operations
+
+Problem	    Operation	                     Method Used	            Alternative Methods
+
+1	        BFS traversal	                 bfs()               	    DFS traversal
+2	        Check bipartiteness using BFS	 isBipartite()	            DFS for bipartite checking
+3       	Longest consecutive path search	 findLongestPath() (DFS)	BFS for path finding
+4	        Connected components	         BFS/DFS             	    Use DFS instead of BFS
+
+
 1,.
 Problem Statement
 
